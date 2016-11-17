b7210b58915adcafabff92c4b557974bc99c0dfc
Matheus Piscioneri <mxczpiscioneri@gmail.com>
2016-11-17 16:17:01 -0200

    Created simple authentication

--- routes/beers.js
+++ routes/beers.js
@@ -51,62 +51,82 @@ exports.findAll = function(req, res) {
 }
 
 exports.addBeer = function(req, res) {
-  var BeerModel = new Beer(req.body);
-  BeerModel.save(function(err, Beer) {
-    if (err) {
-      res.status(500);
-      res.json({
-        type: false,
-        data: "Error occured: " + err
-      });
-    } else {
-      res.json({
-        type: true,
-        data: Beer
-      });
-    }
-  });
-}
-
-exports.updateBeer = function(req, res) {
-  var updatedBeerModel = new Beer(req.body);
-  console.log(req.body);
-  Beer.findByIdAndUpdate(new ObjectId(req.params.id), updatedBeerModel, function(err, Beer) {
-    if (err) {
-      res.status(500);
-      res.json({
-        type: false,
-        data: "Error occured: " + err
-      });
-    } else {
-      if (Beer) {
+  if (authorization(res, req.headers.authorization)) {
+    var BeerModel = new Beer(req.body);
+    BeerModel.save(function(err, Beer) {
+      if (err) {
+        res.status(500);
+        res.json({
+          type: false,
+          data: "Error occured: " + err
+        });
+      } else {
         res.json({
           type: true,
           data: Beer
         });
-      } else {
+      }
+    });
+  }
+}
+
+exports.updateBeer = function(req, res) {
+  if (authorization(res, req.headers.authorization)) {
+    var updatedBeerModel = new Beer(req.body);
+    console.log(updatedBeerModel);
+    Beer.findByIdAndUpdate(new ObjectId(req.params.id), updatedBeerModel, function(err, Beer) {
+      if (err) {
+        res.status(500);
         res.json({
           type: false,
-          data: "Beer: " + req.params.id + " not found"
+          data: "Error occured: " + err
         });
+      } else {
+        if (Beer) {
+          res.json({
+            type: true,
+            data: Beer
+          });
+        } else {
+          res.json({
+            type: false,
+            data: "Beer: " + req.params.id + " not found"
+          });
+        }
       }
-    }
-  });
+    });
+  }
 }
 
 exports.deleteBeer = function(req, res) {
-  Beer.findByIdAndRemove(new Object(req.params.id), function(err, Beer) {
-    if (err) {
-      res.status(500);
-      res.json({
-        type: false,
-        data: "Error occured: " + err
-      });
-    } else {
-      res.json({
-        type: true,
-        data: "Beer: " + req.params.id + " deleted successfully"
-      });
-    }
-  });
+  if (authorization(res, req.headers.authorization)) {
+    Beer.findByIdAndRemove(new Object(req.params.id), function(err, Beer) {
+      if (err) {
+        res.status(500);
+        res.json({
+          type: false,
+          data: "Error occured: " + err
+        });
+      } else {
+        res.json({
+          type: true,
+          data: "Beer: " + req.params.id + " deleted successfully"
+        });
+      }
+    });
+  }
+}
+
+function authorization(res, headersAuthorization) {
+  var auth = new Buffer(process.env.AUTH_USER + ":" + process.env.AUTH_PASS).toString("base64");
+
+  if (auth != headersAuthorization) {
+    res.status(401);
+    res.json({
+      type: false,
+      data: "Erro 401: UNAUTHORIZED ACCESS"
+    });
+    return false;
+  }
+  return true;
 }
